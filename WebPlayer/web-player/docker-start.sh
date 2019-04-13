7f35381563236e3e0f35619978da103da3f5ef10
Karl-Oskar Smed <karloskarsmed89@gmail.com>
2019-04-13 17:33:56 +0200

    Made nginx conf override toggleable

--- WebPlayer/web-player/docker-start.sh
+++ WebPlayer/web-player/docker-start.sh
@@ -5,5 +5,11 @@ cat <<EOF >./config.js
 window.STREAMER_API_URL='${RUNTIME_STREAMER_API_URL}';
 EOF
 
+# copy nginx conf if needed
+if [ "$API_PROXY" = "true" ]
+then
+  cp /nginx.conf /etc/nginx/conf.d/default.conf;
+fi
+
 # start using args
 exec "$@";
